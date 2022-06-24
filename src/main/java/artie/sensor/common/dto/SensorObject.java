50a4b209009255e426164544b8806c65ccfb75a6
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-06-24 19:21:22 +0200

    Added Lombok and fixed the date time error.

--- src/main/java/artie/sensor/common/dto/SensorObject.java
+++ src/main/java/artie/sensor/common/dto/SensorObject.java
@@ -3,70 +3,32 @@ package artie.sensor.common.dto;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import artie.sensor.common.enums.SensorObjectTypeEnum;
 
 @Component
+@Data
+@NoArgsConstructor
 public class SensorObject {
 	//Attributes
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy, hh:mm:ss")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy, hh:mm:ss 'UTC'")
 	protected Date date;
 	protected long milliseconds;
 	protected Object data;
 	protected SensorObjectTypeEnum sensorObjectType;
 	protected String sensorName;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy, hh:mm:ss")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy, hh:mm:ss 'UTC'")
 	protected Date fromDate;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy, hh:mm:ss")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy, hh:mm:ss 'UTC'")
 	protected Date toDate;
 	
 	//Properties
-	public Date getDate(){
-		return this.date;
-	}
 	public void setDate(Date date){
 		this.date = date;
 		this.milliseconds = this.date.getTime();
-	}
-	public Object getData(){
-		return this.data;
-	}
-	public void setData(Object data){
-		this.data = data;
-	}
-
-	public long getMilliseconds(){
-		return this.milliseconds;
-	}
-	public void setMilliseconds(long milliseconds) {
-		this.milliseconds = milliseconds;
-	}
-	
-	public SensorObjectTypeEnum getSensorObjectType(){
-		return this.sensorObjectType;
-	}
-	public void setSensorObjectType(SensorObjectTypeEnum sensorObjectType){
-		this.sensorObjectType = sensorObjectType;
-	}
-	
-	public String getSensorName(){
-		return this.sensorName;
-	}
-	public void setSensorName(String sensorName){
-		this.sensorName = sensorName;
-	}
-
-	public Date getFromDate(){return this.fromDate;}
-	public void setFromDate(Date fromDate){this.fromDate = fromDate;}
-
-	public Date getToDate(){return this.toDate;}
-	public void setToDate(Date toDate){this.toDate = toDate;}
-
-	/**
-	 * Default constructor
-	 */
-	public SensorObject(){
 	}
 	
 	/**
